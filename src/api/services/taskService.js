af98922ebf39e59bd99c2a79a9346231f1085bee
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-04-26 22:18:31 +0100

    adds dotenv support

--- src/api/services/taskService.js
+++ src/api/services/taskService.js
@@ -1,11 +1,11 @@
-
+const _de = require('dotenv').config();
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'api',
-  password: 'password',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
 
 
