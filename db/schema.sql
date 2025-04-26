4542cb39b96052c093d52008a186645db9d9a7b1
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-04-26 22:10:25 +0100

    updates task schema for demo data

--- db/schema.sql
+++ db/schema.sql
@@ -5,3 +5,8 @@ CREATE TABLE tasks (
     status VARCHAR(50) NOT NULL,
     due_date TIMESTAMP
 );
+
+INSERT INTO tasks (title, description, status, due_date)
+    VALUES
+    ('demo task', 'sample task for database testing', 'complete', '2025-04-28'),
+    ('example task', 'you need example tasks', 'complete', '2025-04-28');
